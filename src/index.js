90720aa4c56652949b6cf132b8ffa552278783df
Dieu <contact@trinhngocdieu.com>
2020-10-21 14:20:47 +0800

    fix: react-pdf using default file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import React, {Component} from 'react';
 import PDFViewer from './PdfViewer';
 
-const PDF_DEMO_LINK = "https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf";
-export default class extends Component {
-  render() {
-    return <div>
-      <PDFViewer
-        url={PDF_DEMO_LINK}
-        showProgressBar
-        showToolbox
-      />
-    </div>
-  }
-}
+export default PDFViewer;
